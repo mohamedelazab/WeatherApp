dfac390c8eeb0119f93805e615c2ac54f3cd9bb9
Mohamed <mohamedelazab348@gmail.com>
2019-03-18 17:33:23 +0200

    update packages structure.

---
+++ app/src/main/java/com/waether/app/features/homeScreen/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.waether.app.Features.homeScreen
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.waether.app.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
